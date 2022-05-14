fcbc1f00d8edf22728920546f7953503b5b31dc9
unknown <diego2002lml@gmail.com>
2022-05-13 20:11:20 -0400

    Listas

---
+++ Objetos.js
@@ -0,0 +1,29 @@
+
+var arreglo = []
+var obj = new Object()
+
+function Example(){
+    let element_name=  document.getElementById("nombre")
+    let element_edad= document.getElementById("edad")
+    
+
+    let contacto = new Object()
+    contacto.nombre= element_name.value
+    contacto.edad = element_edad.value
+
+    
+    let variable2 = {
+        nombre: element_name.value ,
+        edad: element_edad.value
+
+    
+    
+    }
+    arreglo.push(contacto)
+    console.log(contacto)
+    console.log(variable2)
+}
+
+function list(){
+    console.log(arreglo)
+}
